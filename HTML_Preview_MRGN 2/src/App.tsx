e5afd5a1839083705b22b1db1c5bf1f4e01b718c
knutoleMRGN <knutole@mrgn.no>
2025-11-21 12:53:54 +0100

    Add files via upload

---
+++ HTML_Preview_MRGN 2/src/App.tsx
@@ -0,0 +1,9 @@
+import { HtmlPreviewTool } from "./components/HtmlPreviewTool";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-100">
+      <HtmlPreviewTool />
+    </div>
+  );
+}
