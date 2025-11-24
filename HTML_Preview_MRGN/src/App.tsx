731d4cd9fccc3c69321d16e1cbc3824a5920da27
knutoleMRGN <knutole@mrgn.no>
2025-11-24 10:05:42 +0100

    Delete HTML_Preview_MRGN directory

--- HTML_Preview_MRGN/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { HtmlPreviewTool } from "./components/HtmlPreviewTool";
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-100">
-      <HtmlPreviewTool />
-    </div>
-  );
-}
